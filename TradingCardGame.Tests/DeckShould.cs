bf87439fd9bda9d2b844985e5520971fc6d5bf95
Ronny <ronnygx@gmail.com>
2019-03-22 17:02:15 +0000

    Green: [TestCase(1, 2)]

--- TradingCardGame.Tests/DeckShould.cs
+++ TradingCardGame.Tests/DeckShould.cs
@@ -11,12 +11,13 @@ namespace TradingCardGame.Tests {
         }
 
         [TestCase(0, 2)]
-        public void deck_must_have_cards_with_same_mana_cost_than_damage(int manaCost, int amountOfCards) {
+        [TestCase(1, 2)]
+        public void deck_must_have_N_cards_with_same_X_mana_cost_than_damage(int manaCost, int amountOfCards) {
             var deck = new Deck();
 
             var damage = manaCost;
-            var dudCards = deck.Cards.Where(card => card.ManaCost == manaCost && card.Damage == damage).ToList();
-            dudCards.Should().HaveCount(amountOfCards);
+            var cards = deck.Cards.Where(card => card.ManaCost == manaCost && card.Damage == damage).ToList();
+            cards.Should().HaveCount(amountOfCards);
         }
     }
 }
--- TradingCardGame/Deck.cs
+++ TradingCardGame/Deck.cs
@@ -3,14 +3,15 @@ using System.Linq;
 
 namespace TradingCardGame {
     public class Deck {
-        private const int NumberOfDudCards = 2;
+        private const int Pair = 2;
         private const int NumberOfTotalCards = 20;
         public List<Card> Cards { get; }
 
         public Deck() {
             Cards = new List<Card>()
-                .Concat(Enumerable.Repeat(new Card(0), NumberOfDudCards))
-                .Concat(Enumerable.Repeat(new Card(10), NumberOfTotalCards - NumberOfDudCards))
+                .Concat(Enumerable.Repeat(new Card(0), Pair))
+                .Concat(Enumerable.Repeat(new Card(1), Pair))
+                .Concat(Enumerable.Repeat(new Card(10), NumberOfTotalCards - Pair - Pair))
                 .ToList();
         }
     }
