d4a8e3298e4a35235e9278c4bdb3c6a224d56b3d
Ronny <ronnygx@gmail.com>
2019-03-22 16:37:50 +0000

    Green: deck_must_have_2_dud_cards

--- TradingCardGame.Tests/DeckShould.cs
+++ TradingCardGame.Tests/DeckShould.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -9,18 +10,34 @@ namespace TradingCardGame.Tests {
             var deck = new Deck();
             deck.Cards.Should().HaveCount(20);
         }
+
+        [Test]
+        public void deck_must_have_2_dud_cards() {
+            var deck = new Deck();
+
+            var dudCards = deck.Cards.Where(card => card.ManaCost == 0 && card.Damage == 0).ToList();
+            dudCards.Should().HaveCount(2);
+        }
     }
 
     public class Deck {
         public List<Card> Cards { get; }
 
         public Deck() {
-            Cards = new List<Card>();
-            for (var i = 0; i < 20; i++) {
-                Cards.Add(new Card());
-            }
+            Cards = new List<Card>()
+                .Concat(Enumerable.Repeat(new Card(0), 2))
+                .Concat(Enumerable.Repeat(new Card(10), 18))
+                .ToList();
         }
     }
 
-    public class Card { }
+    public class Card {
+        public int ManaCost { get; }
+        public int Damage { get; }
+
+        public Card(int manaCost) {
+            ManaCost = manaCost;
+            Damage = ManaCost;
+        }
+    }
 }
