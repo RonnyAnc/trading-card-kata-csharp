85e405f9b28458cbb3ef5c3725912479f22a88fa
Ronny Ancorini <ronnygx@gmail.com>
2019-03-22 20:24:33 +0000

    Green: start_with_initial_health

---
+++ TradingCardGame.Tests/PlayerShould.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TradingCardGame.Tests {
+    public class PlayerShould {
+        [Test]
+        public void start_with_full_health() {
+            var player = Player.Create();
+            player.Health.Should().Be(30);
+        }
+    }
+
+    public class Player {
+        private const int InitialHealth = 30;
+        public int Health { get; }
+
+        private Player() {
+            Health = InitialHealth;
+        }
+
+        public static Player Create() {
+            return new Player();
+        }
+    }
+}
