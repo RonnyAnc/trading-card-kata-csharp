8726624a5b9f74ea7b2e72a6a68c9e701addddd6
Ronny <ronnygx@gmail.com>
2019-03-22 17:08:15 +0000

    Green: Test cases

--- TradingCardGame.Tests/DeckShould.cs
+++ TradingCardGame.Tests/DeckShould.cs
@@ -12,6 +12,13 @@ namespace TradingCardGame.Tests {
 
         [TestCase(0, 2)]
         [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 4)]
+        [TestCase(4, 3)]
+        [TestCase(5, 2)]
+        [TestCase(6, 2)]
+        [TestCase(7, 1)]
+        [TestCase(8, 1)]
         public void deck_must_have_N_cards_with_same_X_mana_cost_than_damage(int manaCost, int amountOfCards) {
             var deck = new Deck();
 
--- TradingCardGame/Deck.cs
+++ TradingCardGame/Deck.cs
@@ -3,15 +3,23 @@ using System.Linq;
 
 namespace TradingCardGame {
     public class Deck {
+        private const int Single = 1;
         private const int Pair = 2;
-        private const int NumberOfTotalCards = 20;
+        private const int Trio = 3;
+        private const int Quartet = 4;
         public List<Card> Cards { get; }
 
         public Deck() {
             Cards = new List<Card>()
                 .Concat(Enumerable.Repeat(new Card(0), Pair))
                 .Concat(Enumerable.Repeat(new Card(1), Pair))
-                .Concat(Enumerable.Repeat(new Card(10), NumberOfTotalCards - Pair - Pair))
+                .Concat(Enumerable.Repeat(new Card(2), Trio))
+                .Concat(Enumerable.Repeat(new Card(3), Quartet))
+                .Concat(Enumerable.Repeat(new Card(4), Trio))
+                .Concat(Enumerable.Repeat(new Card(5), Pair))
+                .Concat(Enumerable.Repeat(new Card(6), Pair))
+                .Concat(Enumerable.Repeat(new Card(7), Single))
+                .Concat(Enumerable.Repeat(new Card(8), Single))
                 .ToList();
         }
     }
