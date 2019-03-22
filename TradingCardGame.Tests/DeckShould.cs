f4c3a246de8195544281c6fc5b8ad9fb88649a78
Ronny <ronnygx@gmail.com>
2019-03-22 16:57:45 +0000

    Refactor: uses testcase

--- TradingCardGame.Tests/DeckShould.cs
+++ TradingCardGame.Tests/DeckShould.cs
@@ -10,12 +10,13 @@ namespace TradingCardGame.Tests {
             deck.Cards.Should().HaveCount(20);
         }
 
-        [Test]
-        public void deck_must_have_2_dud_cards() {
+        [TestCase(0, 2)]
+        public void deck_must_have_cards_with_same_mana_cost_than_damage(int manaCost, int amountOfCards) {
             var deck = new Deck();
 
-            var dudCards = deck.Cards.Where(card => card.ManaCost == 0 && card.Damage == 0).ToList();
-            dudCards.Should().HaveCount(2);
+            var damage = manaCost;
+            var dudCards = deck.Cards.Where(card => card.ManaCost == manaCost && card.Damage == damage).ToList();
+            dudCards.Should().HaveCount(amountOfCards);
         }
     }
 }
