653dce5cb979ce1c87b290eb0f35840e7e0ce286
Ronny <ronnygx@gmail.com>
2019-03-22 16:12:02 +0000

    Green: deck_must_have_20_cards

---
+++ TradingCardGame.Tests/DeckShould.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TradingCardGame.Tests {
+    public class DeckShould {
+        [Test]
+        public void deck_must_have_20_cards() {
+            var deck = new Deck();
+            deck.Cards.Should().HaveCount(20);
+        }
+    }
+
+    public class Deck {
+        public List<Card> Cards { get; }
+
+        public Deck() {
+            Cards = new List<Card>();
+            for (var i = 0; i < 20; i++) {
+                Cards.Add(new Card());
+            }
+        }
+    }
+
+    public class Card { }
+}
